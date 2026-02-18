7aa99107bc5854c10a95745db79a89d2ce2fc799
BertieBuitendag <45890771+BertieBuitendag@users.noreply.github.com>
2026-02-18 05:53:27 +0200

    Add files via upload

---
+++ sw_landscape.js
@@ -0,0 +1,18 @@
+const CACHE = 'manie-v3-landscape';
+const FILES = [
+  './ManieMalan_PWA_V3_Landscape.html',
+  './icon-512.png',
+  './manifest_landscape.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
